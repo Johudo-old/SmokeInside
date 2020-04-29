05826e1b92876175d0db39d648d474b2975ae3e9
Johudo <dimazuev11@gmail.com>
2020-04-29 06:28:02 +0300

    Changed content for SEO

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,19 +3,17 @@
 
 	var navigation = document.querySelector('.navigation');
 	var items = navigation.querySelectorAll('span');
-
-
-	items.forEach( function(element) {
+	
+	items.forEach(function (element) {
 		element.onclick = function (event) {
-
 			var containerID = '#' + event.target.attributes['data-scroll-to'].value;
 			var container = $(containerID);
 
 			if (container.length === 1) {
-				$("html,body").animate({scrollTop: container.offset().top - $('header').height() });
+				$("html,body").animate({
+					scrollTop: container.offset().top - $('header').height()
+				});
 			}
-
-		}
+		};
 	});
-
 })();
