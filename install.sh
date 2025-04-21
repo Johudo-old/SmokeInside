6e930e45f166627d832f0dccc9d9e6dcbb561900
Johudo <zuev.dmtriy@gmail.com>
2025-04-21 21:00:47 +0300

    Fix

--- install.sh
+++ install.sh
@@ -12,3 +12,12 @@ sudo ufw default deny
 sudo ufw allow 'Nginx HTTP'
 
 sudo systemctl restart nginx
+
+sudo apt update
+sudo apt install certbot python3-certbot-nginx
+
+sudo certbot --nginx -d smoke-inside.ru -d www.smoke-inside.ru
+
+sudo cp -rf ./smoke-inside-ssl.nginx /etc/nginx/sites-available/smoke-inside.nginx
+
+sudo systemctl restart nginx
